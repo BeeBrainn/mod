ffb4c82f6cc8e07236717baf27e496fa36e5d095
hoody <valera182@gmail.com>
2012-09-24 13:55:12 +0300

    products table layout mod1

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,5 @@
 class PagesController < ApplicationController
   def home
     @products = Product.all
-    @product_types = ProductType.all
-    @product_colors = ProductColor.all
-    @product_sizes = ProductSize.order("number ASC")
   end
 end
