b3e8b737b7034d515dab1d8002647c6a92962b32
hoody <valera182@gmail.com>
2012-10-02 13:31:51 +0300

    users + heroku2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
   	user = User.find_by_email(params[:email])
   	if user and user.authenticate(params[:password])
   		session[:user_id] = user.id
-  		redirect_to edit_user_path(user)
+  		redirect_to "/", alert: "Hello #{user.name}"
   	else
   		redirect_to '/login', alert: "Wrong email or pass"
   	end
